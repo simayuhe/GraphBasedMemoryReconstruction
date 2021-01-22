c49aad7dd68a15fcfa87533157bc949b9ba00b15
kangyongxin <kangyongxin2018@ia.ac.cn>
2021-01-22 20:14:08 +0800

    envtest

--- Envtest/testatari.py
+++ Envtest/testatari.py
@@ -1,17 +1,47 @@
 import gym
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+import numpy as np
+
+def greyscale_preprocessor(state):
+    #state = cv2.cvtColor(state,cv2.COLOR_BGR2GRAY)/255.
+    state = np.dot(state[...,:3], [0.299, 0.587, 0.114])
+    return state
+
+def deepmind_preprocessor1(state):
+    state = greyscale_preprocessor(state)
+    #state = np.array(cv2.resize(state, (84, 84)))
+    #resized_screen = scipy.misc.imresize(state, (110,84))
+    resized_screen = resize(state, (110,84))
+    state = resized_screen[8:92, :]
+    return state
+
+def deepmind_preprocessor(state):
+    state = greyscale_preprocessor(state)
+    #state = np.array(cv2.resize(state, (84, 84)))
+    #resized_screen = scipy.misc.imresize(state, (110,84))
+    resized_screen = resize(state, (110,84))
+    state = resized_screen[18:102, :]
+    return state
 
 def testenvs():
     #env = gym.make("CartPole-v1")
     #env = gym.make("MsPacman-v0")
-    env = gym.make("Alien-v0")
+    name = "HeroNoFrameskip-v4"
+    env = gym.make(name)
     observation = env.reset()
     print("observation",observation.shape)
-    for _ in range(100):
-        env.render()
+   
+    for _ in range(10000):
+        #env.render()
         action = env.action_space.sample() # your agent here (this takes random actions)
         observation, reward, done, info = env.step(action)
         print("reward",reward)
         if done:
+            a = deepmind_preprocessor1(observation)
+            plt.imshow(a)
+            plt.savefig('./tmp/'+name+'re0.png')
+            plt.close()
             observation = env.reset()
     env.close()
 
